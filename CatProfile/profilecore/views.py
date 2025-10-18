78c9d6bb70b7c43ce17c508a4a7ce6c00c24a488
Oduks <charlestrawb_odus@yahoo.com>
2025-10-18 03:20:27 +0100

    Initial commit - Django CatProfile API

---
+++ CatProfile/profilecore/views.py
@@ -0,0 +1,26 @@
+import requests
+from django.http import JsonResponse
+from datetime import datetime, timezone
+
+def me(request):
+    try:
+        response = requests.get("https://catfact.ninja/fact", timeout=5)
+        if response.status_code == 200:
+            cat_fact = response.json().get("facts", "No fact available right now.")
+        else:
+            cat_fact = "Could not fetch a cat fact at the moment."
+    except requests.RequestException:
+        cat_fact = "External API error: Unable to fetch cat fact."
+    
+    data = {
+        "status": "success",
+        "user": {
+            "email": "charlestrawb_odus@yahoo.com",
+            "name": "Odukoya Charles",
+            "stack": "python/django",
+        },
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "fact": cat_fact
+    }
+
+    return JsonResponse(data, content_type = "application/json")
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # hng-cat-stgone
+
+# Stage Zero Backend Task - /me Endpoint
+
+## Overview
+
+This project implements a RESTful API endpoint that returns user profile info and a random cat fact.
+
+## Endpoint
+
+**GET** `/me`
+
+### Response
+
+```json
+{
+  "status": "success",
+  "user": {
+    "email": "your_email@example.com",
+    "name": "Your Full Name",
+    "stack": "Python/Django"
+  },
+  "timestamp": "2025-10-17T12:00:00.000Z",
+  "fact": "Cats sleep for 70% of their lives."
+}
+```
